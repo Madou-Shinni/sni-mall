656ff3087fb275e57e9b2b6fdb2b9d286c62f917
xw <xw188@yum.com>
2022-10-03 13:37:38 +0800

    【add】 1.测试登录接口

--- controllers/admin/loginController.go
+++ controllers/admin/loginController.go
@@ -13,23 +13,18 @@ type LoginController struct {
 
 // Login 登录
 func (con LoginController) Login(c *gin.Context) {
-	con.Success(c)
+	// 1.获取请求参数
+	captchaId := c.PostForm("captchaId")
+	verifyValue := c.PostForm("verifyValue")
+	// 2.验证验证码
+	if flag := models.CaptVerify(captchaId, verifyValue); flag == false {
+		c.String(http.StatusOK, "验证失败！")
+	}
+	c.String(http.StatusOK, "验证成功!")
 }
 
 // Captcha 获取验证码
 func (con LoginController) Captcha(c *gin.Context) {
-	id, b64s, err := models.CaptMake()
-	if err != nil {
-		fmt.Println(err)
-	}
-	c.JSON(http.StatusOK, gin.H{
-		"captchaId":    id,
-		"captchaImage": b64s,
-	})
-}
-
-// CaptVerify 验证验证码
-func (con LoginController) CaptVerify(c *gin.Context) {
 	id, b64s, err := models.CaptMake()
 	if err != nil {
 		fmt.Println(err)
