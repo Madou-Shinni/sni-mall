3bde3f4a90dd8b3498ed0870d0efab2e974f9daf
xw <xw188@yum.com>
2022-10-08 12:09:24 +0800

    【add】 1.引入cors，配置允许跨域访问

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"xiaomi-mall/routers"
 )
@@ -8,7 +9,8 @@ import (
 func main() {
 	// 创建默认路由引擎
 	r := gin.Default()
-
+	// 允许跨域
+	r.Use(cors.Default())
 	// 基于cookie的存储引擎，security111 参数是密钥
 	//store := cookie.NewStore([]byte("security111"))
 	// 配置session中间件,store 是前面配置的存储引擎,我们可以替换成其他存储引擎
